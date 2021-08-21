d0dc7a0cbb38d0cb5ad46ce19ef5f31cf34332bd
Samama Majeed <samama0096@gmail.com>
2021-08-21 18:57:01 +0500

    fOne

--- app/main.js
+++ app/main.js
@@ -1,17 +1,21 @@
 const express = require('express');
+const dotenv = require('dotenv')
 const mongoose = require('mongoose');
 const userroutes = require('../routes/userroutes');
 const blogroutes = require('../routes/blogroutes');
 const profileroutes = require('../routes/profileroutes')
 const port = process.env.PORT || 2000;
+dotenv.config();
 const app = express();
 app.use(express.json());
 
 
 
+
 //connection to mongo database :
-mongoose.connect("mongodb+srv://samo:msaleh0096@bloguser.uhgmq.mongodb.net/bloguser?retryWrites=true&w=majority", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, () => {
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, () => {
     console.log("connected to mongo");
+
 });
 
 
